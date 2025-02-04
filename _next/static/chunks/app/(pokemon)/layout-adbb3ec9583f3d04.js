8646d3f417b21543c7d95467e117b90182f934c2
nadesh52 <worldaroundmee@gmail.com>
2025-02-04 13:01:32 +0700

    Updates

---
+++ _next/static/chunks/app/(pokemon)/layout-adbb3ec9583f3d04.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[939,57],{34415:function(n,e,u){Promise.resolve().then(u.t.bind(u,34332,23))},34332:function(){}},function(n){n.O(0,[228,971,23,744],function(){return n(n.s=34415)}),_N_E=n.O()}]);
