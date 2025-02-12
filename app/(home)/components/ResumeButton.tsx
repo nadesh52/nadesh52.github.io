d6cd60cee759a71c52c29b785225453c2b6b58b7
nadesh52 <worldaroundmee@gmail.com>
2025-02-12 14:42:45 +0700

    add resume

--- app/(home)/components/ResumeButton.tsx
+++ app/(home)/components/ResumeButton.tsx
@@ -6,7 +6,7 @@ const ResumeButton = () => {
   return (
     <div className="my-8 flex w-fit items-center gap-2 hover:text-type-fire-normal">
       <DocumentTextIcon className="size-6" />
-      <Link href="./hong52.pdf" target="_blank" className="font-mono text-lg">
+      <Link href="./Adoolwit_Resume.pdf" target="_blank" className="font-mono text-lg">
         Resume PDF
       </Link>
     </div>
